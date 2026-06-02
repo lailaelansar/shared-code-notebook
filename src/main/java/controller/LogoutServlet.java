8c111fbe5fbae854b59145d75b07495f868bd301
Laila El Ansar <lailaelansar@gmail.com>
2026-06-02 14:56:46 +0100

    Log: record calls to logout endpoint and accept GET/POST

--- src/main/java/controller/LogoutServlet.java
+++ src/main/java/controller/LogoutServlet.java
@@ -1,8 +1,10 @@
 package controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -11,8 +13,11 @@ import java.util.Map;
 @RequestMapping("/api/auth")
 public class LogoutServlet {
 
-    @PostMapping("/logout")
+    private static final Logger LOG = LoggerFactory.getLogger(LogoutServlet.class);
+
+    @RequestMapping(value = "/logout", method = {RequestMethod.POST, RequestMethod.GET})
     public ResponseEntity<?> logout() {
+        LOG.info("Logout endpoint called");
         return ResponseEntity.ok(Map.of("message", "Logged out successfully"));
     }
 }
